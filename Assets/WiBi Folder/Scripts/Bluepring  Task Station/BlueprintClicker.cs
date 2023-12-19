6f42be6425ddafb0adecb9aae66b6a977712d884
Liam <wliam4410@gmail.com>
2023-12-19 11:27:02 +0000

    Blueprint Station Built
    - Built the Station basics.
    - Added blueprint randomiser too

---
+++ Assets/WiBi Folder/Scripts/Bluepring  Task Station/BlueprintClicker.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueprintClicker : MonoBehaviour
+{
+    [SerializeField] bool alreadyGotBlueprint;
+    [SerializeField] GameObject cam;
+    [SerializeField] LayerMask blueprint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            alreadyGotBlueprint = false;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, blueprint))
+        {
+            //Debug.Log("Hit");
+
+            Debug.DrawRay(cam.transform.position, cam.transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            if (Input.GetButtonDown("Fire"))
+            {
+                //Debug.Log("Hit after clicked");
+                if (hit.collider != null)
+                {
+                    if (!alreadyGotBlueprint)
+                    {
+                        // Code to call function on blueprint which gives the player the blueprint
+                        int b = hit.collider.gameObject.GetComponent<BlueprintID>().id;
+                        hit.collider.gameObject.GetComponent<BlueprintGiver>().Blueprints(b);
+                        alreadyGotBlueprint = true;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Assets/WiBi Folder/Scripts/Bluepring  Task Station/BlueprintGiver.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script was created by Liam Wilson.
+// For the purpose of assigning the player blueprints to build.
+
+public class BlueprintGiver : MonoBehaviour
+{
+    [SerializeField] GameObject[] blueprint;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            blueprint[0].SetActive(false);
+            blueprint[1].SetActive(false);
+        }
+    }
+    public void Blueprints(int b)
+    {
+        if(b == 250)
+        {
+            int r = Random.Range(0, blueprint.Length);
+            blueprint[r].SetActive(true);
+        }
+        else if (b == 0)
+        {
+            // Give Train
+            Debug.Log("Train Blueprint Given");
+            blueprint[b].SetActive(true);
+        }
+        else if (b == 1)
+        {
+            // Give Bear
+            Debug.Log("Bear Blueprint Given");
+            blueprint[b].SetActive(true);
+        }
+    }
+}
