55e3ea98ba00fce536cf8fc276dd932c75207882
ThomasCain7i <115077729+ThomasCain7i@users.noreply.github.com>
2023-12-23 13:35:36 +0000

    Item box new art and audio main

---
+++ Assets/Wreckz Folder/Scripts/SingletonAudioManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SingletonAudioManager : MonoBehaviour
+{
+    // Audio players components.
+    public AudioSource EffectsSource;
+    public AudioSource MusicSource;
+
+    // Random pitch adjustment range.
+    public float LowPitchRange = .95f;
+    public float HighPitchRange = 1.05f;
+
+    // Singleton instance.
+    public static SingletonAudioManager Instance = null;
+
+    // Initialize the singleton instance.
+    private void Awake()
+    {
+
+
+        // If there is not already an instance of SoundManager, set it to this.
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        //If an instance already exists, destroy whatever this object is to enforce the singleton.
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+    }
+}
